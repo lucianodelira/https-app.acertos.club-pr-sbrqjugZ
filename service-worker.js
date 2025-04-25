3b3d8a885d3c5a366cadced41a6b6d5c9aa39dc6
lucianodelira <138644864+lucianodelira@users.noreply.github.com>
2025-04-25 18:00:31 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,2 @@
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('acertos-cache').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
